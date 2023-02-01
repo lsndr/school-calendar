c1984b3b91a4683765ce0db69d43505e89b7f434
lsndr <alexander@lsndr.me>
2023-02-01 12:45:04 +0400

    feat: add outbox

---
+++ migrations/Migration20230201082844.ts
@@ -0,0 +1,21 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20230201082844 extends Migration {
+  async up(): Promise<void> {
+    const knex = this.getKnex();
+
+    return knex.schema.createTable('outbox', (table) => {
+      table.text('id').primary();
+      table.text('topic').notNullable();
+      table.jsonb('payload').notNullable().index();
+      table.dateTime('created_at', { useTz: false }).notNullable();
+      table.dateTime('processed_at', { useTz: false });
+    });
+  }
+
+  override async down(): Promise<void> {
+    const knex = this.getKnex();
+
+    return knex.schema.dropTable('outbox');
+  }
+}
--- src/modules/scheduler/database/index.ts
+++ src/modules/scheduler/database/index.ts
@@ -1,4 +1,5 @@
 export * from './teacher';
 export * from './school';
 export * from './group';
+export * from './outbox';
 export * from './subject';
