b8c3765b7b3d399535d8363bbce375c5893e1cf0
Alexander <alexander@lsndr.me>
2023-02-05 20:04:57 +0000

    feat: allow to search for multiple schools

--- src/modules/scheduler/application/schools/schools.service.test.ts
+++ src/modules/scheduler/application/schools/schools.service.test.ts
@@ -3,12 +3,14 @@ import { Test } from '@nestjs/testing';
 import { testMikroormProvider } from '../../../../../test-utils';
 import { MikroORM } from '@mikro-orm/postgresql';
 import { CreateSchoolDto } from './create-school.dto';
+import { School, SchoolId, TimeZone } from '../../domain';
+import { DateTime } from 'luxon';
 
 describe('Schools Service', () => {
   let schoolsService: SchoolsService;
   let orm: MikroORM;
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     const moduleRef = await Test.createTestingModule({
       controllers: [],
       providers: [SchoolsService, testMikroormProvider],
@@ -36,7 +38,55 @@ describe('Schools Service', () => {
     });
   });
 
-  afterAll(async () => {
+  it('should find schools', async () => {
+    await seed(orm);
+
+    const result = await schoolsService.findMany();
+
+    expect(result).toEqual([
+      {
+        id: expect.any(String),
+        name: 'School 1',
+        timeZone: 'Europe/Moscow',
+      },
+      {
+        id: expect.any(String),
+        name: 'School 2',
+        timeZone: 'Europe/London',
+      },
+    ]);
+  });
+
+  afterEach(async () => {
     await orm.close();
   });
 });
+
+async function seed(orm: MikroORM) {
+  const em = orm.em.fork();
+
+  const school1 = School.create({
+    id: SchoolId.create(),
+    name: 'School 1',
+    timeZone: TimeZone.create('Europe/Moscow'),
+    now: DateTime.fromISO('2023-02-05T19:48:34', {
+      zone: 'Europe/Moscow',
+    }),
+  });
+
+  const school2 = School.create({
+    id: SchoolId.create(),
+    name: 'School 2',
+    timeZone: TimeZone.create('Europe/London'),
+    now: DateTime.fromISO('2023-02-07T10:12:56', {
+      zone: 'Europe/London',
+    }),
+  });
+
+  await em.persistAndFlush([school1, school2]);
+
+  return {
+    school1,
+    school2,
+  };
+}
--- src/modules/scheduler/application/schools/schools.service.ts
+++ src/modules/scheduler/application/schools/schools.service.ts
@@ -53,4 +53,26 @@ export class SchoolsService {
       timeZone: record.time_zone,
     });
   }
+
+  async findMany() {
+    const knex = this.orm.em.getConnection().getKnex();
+
+    const records = await knex
+      .select(['id', 'time_zone', 'name'])
+      .from('school');
+
+    const schools: SchoolDto[] = [];
+
+    for (const record of records) {
+      schools.push(
+        new SchoolDto({
+          id: record.id,
+          name: record.name,
+          timeZone: record.time_zone,
+        }),
+      );
+    }
+
+    return schools;
+  }
 }
