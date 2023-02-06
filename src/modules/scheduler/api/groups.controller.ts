74a551be521ba68220e616efde929f11fbc9880c
Alexander <alexander@lsndr.me>
2023-02-06 19:10:21 +0000

    feat(scheduler): add endpoint to find one group

--- src/modules/scheduler/api/groups.controller.ts
+++ src/modules/scheduler/api/groups.controller.ts
@@ -1,5 +1,17 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiOkResponse } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiOkResponse,
+  ApiNotFoundResponse,
+} from '@nestjs/swagger';
 import { GroupDto, GroupsService, CreateGroupDto } from '../application';
 
 @ApiTags('Groups')
@@ -22,5 +34,22 @@ export class GroupsController {
     @Body() dto: CreateGroupDto,
   ): Promise<GroupDto> {
     return this.groupsService.create(schoolId, dto);
+  }
+
+  @ApiOperation({ operationId: 'findById' })
+  @ApiOkResponse({ type: GroupDto })
+  @ApiNotFoundResponse()
+  @Get('/:id')
+  async findById(
+    @Param('schoolId') schoolId: string,
+    @Param('id') id: string,
+  ): Promise<GroupDto> {
+    const group = await this.groupsService.findOne(schoolId, id);
+
+    if (!group) {
+      throw new NotFoundException();
+    }
+
+    return group;
   }
 }
