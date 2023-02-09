d565a6e14b18ba265b2a07a35cb87b6d149d3e58
lsndr <alexander@lsndr.me>
2023-02-09 07:48:44 +0000

    fix(shared): make events manager initalising on demand

--- src/modules/shared/domain/aggregate-root.ts
+++ src/modules/shared/domain/aggregate-root.ts
@@ -1,7 +1,19 @@
 import { AggregateEvents } from './aggregate-events';
 
 export abstract class AggregateRoot {
-  protected _eventsManager = new AggregateEvents();
+  private __eventsManager?: AggregateEvents;
+
+  protected get _eventsManager() {
+    if (!this.__eventsManager) {
+      this.__eventsManager = new AggregateEvents();
+    }
+
+    return this.__eventsManager;
+  }
+
+  protected set _eventsManager(em: AggregateEvents) {
+    this.__eventsManager = em;
+  }
 
   get events() {
     return this._eventsManager.events;
