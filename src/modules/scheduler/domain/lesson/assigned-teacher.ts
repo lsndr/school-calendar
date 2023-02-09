2d65b3d60cf54bd032ec8ba611147b4fc8dd4ef5
lsndr <alexander@lsndr.me>
2023-02-09 08:27:33 +0000

    fix(scheduler): use assigned teacher id

--- src/modules/scheduler/domain/lesson/assigned-teacher.ts
+++ src/modules/scheduler/domain/lesson/assigned-teacher.ts
@@ -1,6 +1,6 @@
 import { Entity, ManyToOne, PrimaryKey, Property, Rel } from '@mikro-orm/core';
 import { DateTime } from 'luxon';
-import { TeacherIdType } from '../../database';
+import { AssignedTeacherIdType, TeacherIdType } from '../../database';
 import { AssignedTeacherId } from './assigned-teacher-id';
 // eslint-disable-next-line import/no-cycle -- Required by MikroORM
 import { Lesson } from './lesson';
@@ -9,7 +9,7 @@ import { DateTimeType } from '../../../shared/database/types';
 
 @Entity({ tableName: 'lesson_teacher', customRepository: () => Object })
 export class AssignedTeacher {
-  @PrimaryKey({ type: TeacherIdType })
+  @PrimaryKey({ type: AssignedTeacherIdType })
   id: AssignedTeacherId;
 
   @Property({ type: TeacherIdType })
